fe49d6230580e14cc95c761ec87cfa14b56ab7a2
Ajoy <ajaykdas97@gmail.com>
2021-08-03 19:54:29 +0530

    tasks schedule

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 import discord
 import requests
-from discord.ext import commands
+from discord.ext import commands ,tasks
 
 client = commands.Bot(command_prefix="!")
 colors = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
@@ -96,14 +96,7 @@ async def news_glob(ctx):
     embedVar.set_image(url = urlImage)
     await ctx.channel.send(embed=embedVar)
 
-@client.command()
-async def news_dev(ctx):
-    embedVar = discord.Embed(title="", description="", color = random.choice(colors))
-    embedVar.add_field(name="Hello, I am a X Æ A-12 Bot.", value="~", inline=False)
-    embedVar.add_field(name="Currently under development.", value="~", inline=False)
-    embedVar.add_field(name="Author :: ΜΔK#8374", value="~", inline=False)
-    embedVar.add_field(name="Prefix: !  [!news]", value="~", inline=False)
-    await ctx.channel.send(embed=embedVar)
+
 
     
 @client.command()
@@ -114,8 +107,46 @@ async def joke(ctx):
     punchline = y["punchline"] 
     embedVar = discord.Embed(title="", description="", color = random.choice(colors))
     embedVar.add_field(name=setup, value=punchline, inline=False)
-    # embedVar.add_field(name="\t~", value=punchline, inline=False)
     await ctx.channel.send(embed=embedVar)
+
+
+
+@tasks.loop(seconds=10)
+async def auto_send():
+    channel = await client.fetch_channel('852579630807777376')
+    url = 'https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=353a4fcdcbe7413b8c99de9a311dcfc6'
+    # Do the HTTP get request
+    response = requests.get(url, verify=True)
+    json_data = json.loads(response.text)
+    
+    
+    total_results = int(json_data['totalResults'])
+    rand = random.randint(1, 10)
+    author = json_data['articles'][rand]['author']
+    title = json_data['articles'][rand]['title']
+    description = json_data['articles'][rand]['description']
+    url = json_data['articles'][rand]['url']
+    urlImage = json_data['articles'][rand]['urlToImage']
+    publishedAt = json_data['articles'][rand]['publishedAt']
+    content = json_data['articles'][rand]['content']
+
+    embedVar = discord.Embed(title=title, description="", color = random.choice(colors))
+    embedVar.add_field(name="Author-", value=author, inline=True)
+    embedVar.add_field(name="---------------------------------", value=url, inline=False)
+    embedVar.add_field(name="---------------------------------", value=description, inline=False)
+    embedVar.add_field(name="---------------------------------", value=publishedAt, inline=False)
+    embedVar.set_image(url = urlImage)
+    await channel.send(embed=embedVar)    
+
+
+@client.command()
+async def news_dev(ctx):
+    embedVar = discord.Embed(title="", description="", color = random.choice(colors))
+    embedVar.add_field(name="Hello, I am a X Æ A-12 Bot.", value="~", inline=False)
+    embedVar.add_field(name="Currently under development.", value="~", inline=False)
+    embedVar.add_field(name="Author :: ΜΔK#8374", value="~", inline=False)
+    embedVar.add_field(name="Prefix: !  [!news]", value="~", inline=False)
+    await ctx.channel.send(embed=embedVar)    
     
    
 
